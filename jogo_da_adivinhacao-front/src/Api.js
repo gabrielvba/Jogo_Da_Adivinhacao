fab7d5c13be157b7946619b05fdc87dcbf8b75bb
Gabriel Valentino <gabriel.alves@ccc.ufcg.edu.br>
2020-10-14 17:52:46 -0300

    Removendo front-end

--- jogo_da_adivinhacao-front/src/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const port = process.env.PORT || '8082';
-
-export default axios.create({
-    baseURL: `http://localhost:${port}/`,
-    headers: { "Content-Type": "application/json" }
-})
